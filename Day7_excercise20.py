0965060b8c4dc14a9a7a3cf077756882649c2517
Trixie <annabeat08@gmail.com>
2026-03-30 22:35:59 +0300

    Indexing and len

--- Day7_excercise20.py
+++ Day7_excercise20.py
@@ -2,16 +2,16 @@ import sys
 
 def main():
 
-    fruit = input("Kindly what is your favourite fruit: ")
-    colour = input("Kindly what is your favourite colour: ")
-    city = input("Kindly what is your favourite city: ")
+    fruit = input("Kindly what is your favourite fruit: ")#user input is stored in a var called fruit
+    colour = input("Kindly what is your favourite colour: ")#user input is stored in a var called colour
+    city = input("Kindly what is your favourite city: ")#user input is stored in a var called city
 
-    words =[fruit, colour, city]
-    print(f"{words}")
+    words =[fruit, colour, city]#My words converted to a list(stored)
+    print(f"{words}")#prints the words in a list form
 
-    print(F"{words[0]}")
-    print(F"{words[2]}")
-    print(F"{len(words)}")
+    print(F"{words[0]}")#prints 1st word on my list
+    print(F"{words[2]}")#prints last word on my list
+    print(F"{len(words)}")#counts my list items
 
 
 
