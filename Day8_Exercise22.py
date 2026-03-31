4a7de038f30a399ef525af3c375f0e98f3eb7dc5
Trixie <annabeat08@gmail.com>
2026-03-31 17:08:47 +0300

    storing numbers in a list and converting then using indexes to int

---
+++ Day8_Exercise22.py
@@ -0,0 +1,22 @@
+import sys
+
+def main():
+
+        number1 = input("Kindly input the first number: ")#asking for user input
+        number2 = input("Kindly input the second number: ")#asking for user input
+        number3 = input("Kindly input the third number: ")#asking for user input
+
+        numbers =[number1, number2, number3]#storing user input into a list
+
+        print(f"{numbers}")#prints the list
+
+        numbers[0]= int(numbers[0])#converts list to int
+        numbers[1] = int(numbers[1])#converts list to int
+        numbers[2] = int(numbers[2])#converts list to int
+
+        print(f"Total = {numbers[0] + numbers[1]+ numbers[2]}")# prints the sum
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
