469ace5a62bc0e192db48ddb695fdfe9d20fac90
Trixie <annabeat08@gmail.com>
2026-03-31 17:25:52 +0300

    creating a list and printing the only required values using indexing and slicing

--- Day8_Exercise23.py
+++ Day8_Exercise23.py
@@ -2,11 +2,11 @@ import sys
 
 def main():
 
-        list = ([20,40,60,80,10,50])#creats a list.
+        list = ([20,40,60,80,100,120])#creats a list.
 
         print(f"{list}")#prints the list.
 
-        new_list = list[1:5]#creats a new list, specifying which numbers to be added to our list using indexing
+        new_list = list[1:5]#creats a new list, slice/specifying which numbers to be added to our list using indexing
         print(f"{new_list}")#prints the middle four numbers of the existing list.
 
 
