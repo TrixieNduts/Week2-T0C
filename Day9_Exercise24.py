a045a199d15227ca245301bae161fe6bbf2a709a
Trixie <annabeat08@gmail.com>
2026-04-02 00:15:57 +0300

    program asks user for 5 random words, then stores them on a list then goes ahead to print the words begin with 'a' or 'A'

--- Day9_Exercise24.py
+++ Day9_Exercise24.py
@@ -2,19 +2,19 @@ import sys
 
 def main():
 
-    word1 = input("Kindly input random word1: ")
-    word2 = input("Kindly input random word2: ")
-    word3 = input("Kindly input random word3: ")
-    word4 = input("Kindly input random word4: ")
-    word5 = input("Kindly input random word5: ")
+    word1 = input("Kindly input random word1: ")#gets use input.
+    word2 = input("Kindly input random word2: ")#gets use input.
+    word3 = input("Kindly input random word3: ")#gets use input.
+    word4 = input("Kindly input random word4: ")#gets use input.
+    word5 = input("Kindly input random word5: ")#gets use input.
 
-    words = [word1, word2, word3, word4, word5]
+    words = [word1, word2, word3, word4, word5]#storing words in a list.
     print(f"{words}")
 
-    a_words = []
+    a_words = []#Empty list initialisation.
 
-    if words[0][0].lower() == 'a':
-        a_words = a_words + [words[0]]
+    if words[0][0].lower() == 'a':#the two indexes get first letter of the word.
+        a_words = a_words + [words[0]]#adds the word to the list if it qualifies the set parameters.
 
     if words[1][0].lower() == 'a':
         a_words = a_words + [words[1]]
