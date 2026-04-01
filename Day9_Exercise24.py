84818c49f193cf7afe4f3b9310eee472bfe992ba
Trixie <annabeat08@gmail.com>
2026-04-02 00:10:08 +0300

    program asks user for 5 random words, then stores them on a list then goes ahead to print the words begin with 'a' or 'A'

--- Day9_Exercise24.py
+++ Day9_Exercise24.py
@@ -11,8 +11,25 @@ def main():
     words = [word1, word2, word3, word4, word5]
     print(f"{words}")
 
-    a_words = words[0].lower() == 'a'
-    print(f"{a_words}")
+    a_words = []
+
+    if words[0][0].lower() == 'a':
+        a_words = a_words + [words[0]]
+
+    if words[1][0].lower() == 'a':
+        a_words = a_words + [words[1]]
+
+    if words[2][0].lower() == 'a':
+        a_words = a_words + [words[2]]
+
+    if words[3][0].lower() == 'a':
+        a_words = a_words + [words[3]]
+
+    if words[4][0].lower() == 'a':
+        a_words = a_words + [words[4]]
+
+
+    print(f"Words beginning with 'a' or 'A': {a_words}")
 
 if __name__ == "__main__":
     sys.exit(main())
