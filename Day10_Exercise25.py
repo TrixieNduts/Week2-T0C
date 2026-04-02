bde9880c853dad4a604b958af5409123c41f25eb
User <annabeat08@gmail.com>
2026-04-03 00:51:25 +0300

    appending a list

--- Day10_Exercise25.py
+++ Day10_Exercise25.py
@@ -5,7 +5,7 @@ def main():
         nums = ([1,2,3])#creats a list.
         print(f"{nums}")#prints the list.
 
-        nums.append(4)
+        nums.append(4)#adds number specified to our list
         print(f"{nums}")
 
 
