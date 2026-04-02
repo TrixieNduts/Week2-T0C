2e6bf8a2afb1f1d3210dac203f7b73cb20394af9
User <annabeat08@gmail.com>
2026-04-03 00:50:55 +0300

    appending a list

---
+++ Day10_Exercise25.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+
+        nums = ([1,2,3])#creats a list.
+        print(f"{nums}")#prints the list.
+
+        nums.append(4)
+        print(f"{nums}")
+
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
